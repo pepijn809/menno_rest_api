a08b648830198b14784ae50920b0d6b16080497e
Pepijn <6955882+pepijn809@users.noreply.github.com>
2020-04-05 12:54:52 +0200

    Added HTTP-BasicAuthenticatie
    HTTPBasicAuth toegevoegd op basis van Flask-HTTPAuth.

--- menno.py
+++ menno.py
@@ -1,19 +1,42 @@
-# Importeer: Flask, JSONify, Requests, DNSpython + Flask_PyMongo en PyMongo
-from flask import Flask, jsonify, request
+# Requirements
+# - Flask, JSONify, Requests, DNSPython, Flask_PyMongo, Flask_HTTPAuth, Werkzeug.Security
+from flask import Flask, jsonify, request, make_response
 from flask_pymongo import PyMongo
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Naam van de Flask applicatie
+# Flask applicatie met HTTPAuth
 app = Flask(__name__)
+auth = HTTPBasicAuth()
 
-# Mongo Database & MongoDB Connection string
-app.config['MONGO_DBNAME'] = 'menno'
-app.config['MONGO_URI'] = 'mongodb+srv://pepijn:pepijn@menno-cluster-miz8h.mongodb.net/menno'
+# Geauthenticeerde gebruikers voor API
+auth_users = {
+    "mike": generate_password_hash("mike"),
+    "pepijn": generate_password_hash("pepijn")
+}
 
-# Variabel PyMongo App
-mongo = PyMongo(app)
+# MongoDB Instellingen
+app.config['MONGO_DBNAME'] = 'menno' # MongoDB naam
+app.config['MONGO_URI'] = 'mongodb+srv://pepijn:pepijn@menno-cluster-miz8h.mongodb.net/menno' # MongoDB bij MongoDB Atlas op AWS
+#app.config['MONGO_URI'] = 'mongodb://mongo0.mikevdbrink.nl:27017,mongo1.mikevdbrink.nl:27017,mongo2.mikevdbrink.nl:27017/menno?replicaSet=cloud' # MongoDB Bij Google Cloud Platform (GPC)
+mongo = PyMongo(app) # Variabel voor PyMongo
+
+# Authenticatie functie - HTTPAuth op basis van 'auth_users' lijst
+@auth.verify_password
+def verify_password(username, password):
+    if username in auth_users:
+        return check_password_hash(auth_users.get(username), password)
+    return False
+
+# / route - API Homepagina
+@app.route('/')
+@auth.login_required
+def index():
+    return "Hoi, %s!" % auth.username()
 
 # Flask Route - Users \ GET all documents from 'Users Collection'.
 @app.route('/users', methods=['GET'])
+@auth.login_required # Require Basic HTTPAuth
 # Function 'get_all_users' - Vraag alle gebruikers op
 def get_all_users():
     framework = mongo.db.users
@@ -21,28 +44,30 @@ def get_all_users():
     output = []
 
     for q in framework.find():
-        output.append({'voornaam': q['voornaam'], 'achternaam': q['achternaam']})
+        output.append({'voornaam': q['voornaam'], 'achternaam': q['achternaam'], 'name': q['name'], 'plaats': q['plaats'], 'provincie': q['provincie'], 'adres': q['adres']})
     # Return the output in JSON
     return jsonify({'result': output})
 
-# Flask Route - Users \ GET specified document based on <name> from 'Users Collection'.
-@app.route('/users/<name>', methods=['GET'])
-# Function 'get_one_user' - Vraag gegevens van 1 gebruiker op
-def get_one_user(name):
+# Flask Route - Users \ GET een specifiek document gebasseerd op de voornaam van een gebruiker in de 'Users Collectie'.
+@app.route('/users/<voornaam>', methods=['GET'])
+@auth.login_required # Require Basic HTTPAuth
+# Function 'get_one_user' - Vraag gegevens van 1 gebruiker op op basis van voornaam
+def get_one_user(voornaam):
     users = mongo.db.users
 
-    q = users.find_one({'voornaam': name})
+    q = users.find_one({'voornaam': voornaam})
 
     if q:
-        output = {'voornaam': q['voornaam'], 'achternaam': q['achternaam']}
+        output = {'voornaam': q['voornaam'], 'achternaam': q['achternaam'], 'name': q['name'], 'plaats': q['plaats'], 'provincie': q['provincie'], 'adres': q['adres']}
     else:
         output = 'Sorry! We konden geen gebruikers vinden.'
     # Return the output in JSON
     return jsonify({'result': output})
 
-# Flask Route - Users \ POST specified document based on <name> and <language> to 'Users Collection'.
+# Flask Route - Users \ POST een specifiek document gebasseerd op gebruikersdetails naar de 'Users Collectie'.
 @app.route('/users', methods=['POST'])
-# Function 'add_user' - Voeg een gebruiker toe op basis van JSON
+@auth.login_required # Require Basic HTTPAuth
+# Function 'add_user' - Voeg een gebruiker toe op basis van JSON data
 def add_user():
     users = mongo.db.users
 
